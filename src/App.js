2f6321c41fd69e9bfec6981307bbd6f30dc8b3a9
Rupinder Singh <>
2019-07-20 18:19:40 +0530

    Add App.js to render the skeleton for the pages * will be used to render common things like navbar and headers to all the pages

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Dashboard from "./pages/dashboard/Dashboard";
+import './app.css';
+
+const App = () => {
+  return (
+      <div className="appContainer">
+        <main>
+          <Dashboard />
+        </main>
+      </div>
+  )
+};
+
+export default App;
