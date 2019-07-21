455319b59df02db7b839f2d652f5c503e6361d31
Rupinder Singh <>
2019-07-21 19:31:42 +0530

    Add country/language select box in header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import styles from './header.module.css';
+import Select from "../select/Select";
+import FlagIcon from "../flagIcon/FlagIcon";
 
 const Header = () => {
+  const languageOptions = [
+    {value: 'en', label: <span><FlagIcon code="gb" className={styles.flagIcon}/>Eng</span> },
+    {value: 'fr', label: <span><FlagIcon code="fr" className={styles.flagIcon}/>French</span> },
+  ];
+
   return (
-      <header>Header</header>
+      <header>
+        <strong>Dashboard</strong>
+        <Select
+            options={languageOptions}
+            className={styles.languageSelect}
+        />
+      </header>
   )
 };
 
---
+++ src/components/select/Select.js
@@ -0,0 +1,78 @@
+import React, {Component} from 'react';
+import * as PropTypes from "prop-types";
+import classNames from 'classnames';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faAngleDown, faAngleUp} from '@fortawesome/free-solid-svg-icons'
+
+import styles from './select.module.scss';
+
+class Select extends Component {
+  state = {
+    isOpen: false,
+    selectedOptionIndex: 0,
+  };
+
+  toggleOpen = () => {
+    const {isOpen} = this.state;
+    this.setState({isOpen: !isOpen});
+  };
+
+  selectOption = (e, index) => {
+    const {
+      options,
+      onClick,
+    } = this.props;
+
+    this.setState({selectedOptionIndex: index}, this.toggleOpen);
+    onClick && onClick(e, options[index]);
+  };
+
+  render() {
+    const {
+      options,
+      placeHolder,
+      className
+    } = this.props;
+
+    const {
+      selectedOptionIndex,
+      isOpen,
+    } = this.state;
+
+    const effectivePlaceholder = placeHolder || options[selectedOptionIndex].label;
+
+    return (
+        <div className={classNames(styles.select, className)}>
+          <button
+              className={styles.placeholder}
+              onClick={this.toggleOpen}
+          >
+            <div className={styles.placeholderLabel}>{effectivePlaceholder}</div>
+            <FontAwesomeIcon icon={isOpen ? faAngleDown : faAngleUp}/>
+          </button>
+          <If condition={isOpen}>
+            <div className={classNames(styles.options)}>
+              {options.map((option, index) =>
+                  <button
+                      key={option.value}
+                      className={styles.option}
+                      onClick={(e) => this.selectOption(e, index)}
+                  >
+                    {option.label}
+                  </button>
+              )}
+            </div>
+          </If>
+        </div>
+    )
+  }
+}
+
+Select.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.shape({
+    value: PropTypes.string,
+    label: PropTypes.node,
+  })),
+};
+
+export default Select;
