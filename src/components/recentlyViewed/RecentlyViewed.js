e1fe8f9742d23bd4946ebfe6a0cf56c2c3a0c875
Rupinder Singh <>
2019-07-26 19:30:11 +0530

    Apply font colors to recently viewed options

--- src/components/recentlyViewed/RecentlyViewed.js
+++ src/components/recentlyViewed/RecentlyViewed.js
@@ -4,15 +4,16 @@ import classNames from 'classnames';
 import styles from './recentlyViewed.module.scss';
 import {faLongArrowAltRight} from "@fortawesome/free-solid-svg-icons";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import Button from "../button/Button";
 
 const RecentlyViewed = ({
     className
 }) => (
     <div className={classNames(styles.container, className)}>
       <div className={styles.header}>Recently Viewed</div>
-      <button className={styles.option}>Overall Performance <FontAwesomeIcon icon={faLongArrowAltRight}/></button>
-      <button className={styles.option}>Invoice #930 <FontAwesomeIcon icon={faLongArrowAltRight}/></button>
-      <button className={styles.option}>Customer: Minerva Viewer <FontAwesomeIcon icon={faLongArrowAltRight}/></button>
+      <Button className={styles.option}>Overall Performance <FontAwesomeIcon icon={faLongArrowAltRight}/></Button>
+      <Button className={styles.option}>Invoice #930 <FontAwesomeIcon icon={faLongArrowAltRight}/></Button>
+      <Button className={styles.option}>Customer: Minerva Viewer <FontAwesomeIcon icon={faLongArrowAltRight}/></Button>
     </div>
 );
 RecentlyViewed.propTypes = {
